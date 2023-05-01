da02b9333156ea798684c78e93d486f7702f8699
hao <2379585921@qq.com>
2023-05-01 10:57:05 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+/**
+ * Created by hao
+ * 2023/5/1 10:41
+ */
+package com.atguigu.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
